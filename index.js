1401422eb1036021459b83810d266388758d1f43
448197683 <448197683@qq.com>
2021-12-02 14:44:11 +0800

    finish

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-const page2 = document.querySelector('.page2');
+const links = document.querySelectorAll('.link');
+const pages = document.querySelectorAll('.page');
+const navBar = document.querySelector('ul');
 
-const x = page2.getBoundingClientRect();
-console.log(x);
+links.forEach((link, index) => {
+  link.addEventListener('click', (e) => {
+    pages[index];
+    window.scrollTo({
+      top: pages[index].offsetTop - navBar.clientHeight,
+      behavior: 'smooth',
+    });
+  });
+});
+
+window.addEventListener('scroll', (e) => {
+  const threshould = window.innerHeight / 2;
+  links.forEach((link, index) => {
+    const pageTop = pages[index].getBoundingClientRect().top;
+    if (pageTop < threshould) {
+      links[index].classList.add('active');
+    } else {
+      links[index].classList.remove('active');
+    }
+  });
+});
